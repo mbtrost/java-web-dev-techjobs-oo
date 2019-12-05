1c8b15b40c9a6e2fdc4d98ec6074db4afb8134a9
mbtrost <maude.trost@gmail.com>
2019-12-04 21:57:55 -0600

    Finished the tests. Still need to make the code DRY.

--- src/org/launchcode/techjobs_oo/Tests/JobTest.java
+++ src/org/launchcode/techjobs_oo/Tests/JobTest.java
@@ -13,14 +13,14 @@ public class JobTest {
     Job test_job_2;
     Job test_job_3;
     Job test_job_4;
+    Job test_job_5;
 
     @Before
     public void testSetUp() {
         test_job_1 = new Job();
         test_job_2 = new Job();
         test_job_3 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
-        test_job_4 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
-
+        test_job_4 = new Job("", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
     }
 
     @Test
@@ -30,16 +30,35 @@ public class JobTest {
 
     @Test
     public void testJobConstructorSetsAllFields () {
-        assertEquals(test_job_3.getName(), "Product tester");
-        assertEquals(test_job_3.getEmployer().getValue(), "ACME");
-        assertEquals(test_job_3.getLocation().getValue(), "Desert");
-        assertEquals(test_job_3.getPositionType().getValue(), "Quality control");
-        assertEquals(test_job_3.getCoreCompetency().getValue(), "Persistence");
+        assertEquals(3, test_job_3.getId());
+        assertEquals("Product tester", test_job_3.getName());
+        assertEquals("ACME", test_job_3.getEmployer().getValue());
+        assertEquals("Desert", test_job_3.getLocation().getValue());
+        assertEquals("Quality control", test_job_3.getPositionType().getValue());
+        assertEquals("Persistence", test_job_3.getCoreCompetency().getValue());
     }
 
     @Test
     public void testJobsForEquality(){
         assertFalse(test_job_3.getId() == test_job_4.getId());
+    }
+
+    @Test
+    public void testToString() {
+        assertEquals("\n" + "ID: " + test_job_3.getId() + "\n" +
+                        "Name: " + "Product tester" + "\n" +
+                        "Employer: " + "ACME" + "\n" +
+                        "Location: " + "Desert" + "\n" +
+                        "Position Type: " + "Quality control" + "\n" +
+                        "Core Competency: " + "Persistence" + "\n",
+                test_job_3.toString());
+        assertEquals("\n" + "ID: " + test_job_4.getId() + "\n" +
+                "Name: " + "Data not available" + "\n" +
+                "Employer: " + "ACME" + "\n" +
+                "Location: " + "Desert" + "\n" +
+                "Position Type: " + "Quality control" + "\n" +
+                "Core Competency: " + "Persistence" + "\n",
+                test_job_4.toString());
     }
 
 }
